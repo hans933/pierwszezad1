0d5818ece30967bdd3e3cfda986b8deb3182286f
s-122-16 <s-122-16@gumiforest.ae.krakow.pl>
2017-03-14 12:32:54 +0100

    Signed-off-by: s-122-16 <s-122-16@gumiforest.ae.krakow.pl>

---
+++ pg-hello-master/www/js/pgb.js
@@ -0,0 +1,32 @@
+function init() {
+	document.addEventListener("deviceready",onDeviceReady, false);
+}
+
+function onDeviceReady() {
+	navigator.notification.beep(1);
+}
+
+function deviceInfo() {
+
+	info =  'Hi, I am your smartphone :-)' + '\n' +
+			'=====' + '\n' +
+			'Device Name    : '     + device.name     + '\n' + 
+			'Device Cordova : '  + device.cordova + '\n' + 
+			'Device Platform: ' + device.platform + '\n' + 
+			'Device UUID    : '     + device.uuid     + '\n' + 
+			'Device Model   : '    + device.model     + '\n' + 
+			'Device Version : '  + device.version  + '\n';
+
+	navigator.notification.alert(info);
+	
+}
+
+function info(){
+	
+	info = 'Krzysztof Foszt '  + '\n' +
+			'=====' + '\n' +
+			'Krasinskiego 4/26' + '\n' +
+			'Tarnów 33-100'+ '\n' +;
+			
+navigator.notification.alert(info);
+}
